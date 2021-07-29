8b84c8f3e5e9a5e0c096848d8db6c404fac0f3fd
PERicci <riccip@hotmail.com>
2021-07-28 21:01:14 -0400

    component add

--- scripts.js
+++ scripts.js
@@ -17,7 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
             // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
             el.classList.toggle('is-active');
             $target.classList.toggle('is-active');
-            $target.classList.toggle('is-pulled-right');
+            $target.classList.toggle('is-pulled-right')
+            ;
             });
         });
     }
